bdde227fa0120a5dad96879d5cffde4580c6fd12
cmc <chenbming@gmail.com>
2024-12-03 16:03:11 +0100

    rounding_results

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,7 +1,7 @@
 SELECT 
   date_date, 
-  ROUND(SUM(turnover),2) AS daily_turnover,
-  ROUND(SUM(purchase_price*qty),2) AS daily_purchase_cost
+  ROUND(SUM(turnover)) AS daily_turnover,
+  ROUND(SUM(purchase_price*qty)) AS daily_purchase_cost
 FROM `learned-raceway-436207-f6.course12.gwz_sales`
 INNER JOIN `course12.gwz_product`
 USING (products_id)
